65c321f1fdd0ce083a55b017b23f06ff54b744d5
Luke Darcy <luke_darcy@yahoo.com>
2026-03-15 19:48:17 -0700

    Setup deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['image.tmdb.org', 'firebasestorage.googleapis.com'],
   },
+  // If your site is at https://<user>.github.io/<repo>/, set basePath to `'/<repo>'`
+  // basePath: process.env.BASE_PATH || '',
 };
 
 module.exports = nextConfig;
