34286c133d0c4620ab3cdb35fc47b79bbbaeec13
Luke Darcy <luke_darcy@yahoo.com>
2026-03-15 20:00:56 -0700

    bug fix and test for deployment workflow

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   images: {
+    unoptimized: true,
     domains: ['image.tmdb.org', 'firebasestorage.googleapis.com'],
   },
   // If your site is at https://<user>.github.io/<repo>/, set basePath to `'/<repo>'`
